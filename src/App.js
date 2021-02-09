9fdfc808657e198814e625001cbee7420fa6fd9a
Akarsh Saraff <saraffakarsh@gmail.com>
2021-02-09 23:09:05 +0000

    header-component addition

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './styles/main.scss';
 
+import { HeaderComponent } from './components/header/HeaderComponent';
+
 function App() {
   return (
     <div className="App">
-     akarsh saraff
+     <HeaderComponent />
     </div>
   );
 }
---
+++ src/components/header/HeaderComponent.jsx
@@ -0,0 +1,11 @@
+import Proptypes from 'prop-types';
+
+export const HeaderComponent = ({
+
+}) => {
+    return <div className="header-main">
+        <section className="header-left-items">left logo / heading</section>
+        <section className="header-center-items"> nav items</section>
+        <section className="header-right-items"> logout / sign up</section>
+    </div>
+}

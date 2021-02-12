1964375f41bf9c27dc9f5092af7d4edf59c35f29
Akarsh Saraff <saraffakarsh@gmail.com>
2021-02-12 21:30:49 +0000

    instagram

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import './styles/main.scss';
 import { HeaderComponent } from './components/header/HeaderComponent';
 import { ContentContainerComponent } from './components/container/ContentContainerComponent';
 import { NewTodo } from './journies/TODOAPP/NewTodo/NewTodo';
+import { SMPage } from './journies/Instagram/SMPage';
 
 function App() {
   return (
     <div className="App">
-     <HeaderComponent />
-     <main className="main-container">
-      <NewTodo />
-     </main>
+      <SMPage />
     </div>
   );
 }
--- src/components/header/HeaderComponent.jsx
+++ src/components/header/HeaderComponent.jsx
@@ -4,12 +4,12 @@ export const HeaderComponent = ({
 
 }) => {
     return <nav className="header-main">
-        <section className="header-left-items">left logo / heading</section>
+        <section className="header-left-items">Quantum</section>
         <section className="header-center-items"> 
             <ul>
-                <li>link 1</li>
-                <li>link 2</li>
-                <li>link 3</li>
+                <li>To Do</li>
+                <li>Example</li>
+                <li>Example</li>
             </ul>
         </section>
         <section className="header-right-items"> <a className="anchor-main" href="#">Login</a></section>
